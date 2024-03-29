9ff79cadfd4b165b80c3bc400dbd4129d1eca9fb
enesozmus <ensozmus@gmail.com>
2024-03-29 14:00:01 +0300

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by enesozmus on 29.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
